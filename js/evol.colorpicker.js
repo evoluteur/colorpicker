47a373a4ea2b8f21339265880aceaeb943b3c5a2
Evoluteur <olivier@evolutility.org>
2013-02-17 04:01:05 -0800

    Version 2.1

--- js/evol.colorpicker.js
+++ js/evol.colorpicker.js
@@ -1,5 +1,5 @@
 /*!
- * Evol.ColorPicker 2.0
+ * Evol.ColorPicker 2.1
  *
  * Copyright (c) 2013, Olivier Giulieri 
  *
@@ -64,7 +64,7 @@ var _idx=0,
 			
 $.widget( "evol.colorpicker", {
 
-	version: '2.0',
+	version: '2.1',
 	
 	options: {
 		color: null, // example default:'#31859B'
