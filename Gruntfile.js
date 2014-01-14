0b8b7793b18ad1379f799955a79cea727bcb74b6
Olivier Giulieri <olivier@evolutility.org>
2014-01-13 22:42:36 -0800

    Now using LESS.
    LESS : The dynamic stylesheet language.

--- Gruntfile.js
+++ Gruntfile.js
@@ -33,6 +33,28 @@ module.exports = function (grunt) {
                 'js/evol.colorpicker.min.js': ['js/evol.colorpicker.js']
                 }
             }
+        },
+
+        // *************************************************************************************
+        //      LESS
+        // *************************************************************************************
+        less: {
+            options: {
+            },
+            dev: {
+                files: {
+                    "css/evol.colorpicker.css": "less/evol.colorpicker.less",
+                    "css/demo.css": "less/demo.less"
+                }
+            },
+            prod: {
+                options: {
+                    yuicompress: true
+                },
+                files: {
+                    "css/evol.colorpicker.min.css": "less/evol.colorpicker.less"
+                }
+            }
         }
 
     });
@@ -40,6 +62,7 @@ module.exports = function (grunt) {
 // Load the plugin that provides the tasks.
     grunt.loadNpmTasks('grunt-contrib-jshint');
     grunt.loadNpmTasks('grunt-contrib-uglify');
+    grunt.loadNpmTasks('grunt-contrib-less');
 
 // Custom tasks
 
@@ -50,10 +73,10 @@ module.exports = function (grunt) {
     grunt.registerTask('default', ['dev']);
 
     // Dev only task(s).
-    grunt.registerTask('dev', ['jshint']);
+    grunt.registerTask('dev', ['jshint', 'less:dev']);
 
     // Prod only task(s).
-    grunt.registerTask('prod', ['jshint', 'uglify']);
+    grunt.registerTask('prod', ['jshint', 'uglify', 'less:prod']);
 
 };
 
