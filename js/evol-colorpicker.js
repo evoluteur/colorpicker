5f7e8331252d60267583f85feb16a153b7e350f0
Olivier Giulieri <ogiulieri@gmail.com>
2016-03-18 22:29:08 -0700

    Version 2.3.6.

--- js/evol-colorpicker.js
+++ js/evol-colorpicker.js
@@ -1,5 +1,5 @@
 /*
- evol-colorpicker 3.2.5
+ evol-colorpicker 3.2.6
  ColorPicker widget for jQuery UI
 
  https://github.com/evoluteur/colorpicker
@@ -68,7 +68,7 @@ var _idx=0,
 
 $.widget( "evol.colorpicker", {
 
-	version: '3.2.5',
+	version: '3.2.6',
 	
 	options: {
 		color: null, // example:'#31859B'
